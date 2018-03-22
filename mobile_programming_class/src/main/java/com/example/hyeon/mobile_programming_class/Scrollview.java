673644f3b13621d614a1c888e41e7c4546427ef4
Jason Jung <hyeonsuns@naver.com>
2018-03-22 16:27:31 +0900

    Today I Learned in Mobile class

---
+++ mobile_programming_class/src/main/java/com/example/hyeon/mobile_programming_class/Scrollview.java
@@ -0,0 +1,13 @@
+package com.example.hyeon.mobile_programming_class;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Scrollview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrollview);
+    }
+}
