6023b759d616a9fe8f1f9c94d7805bcd8125ef93
Jason Jung <hyeonsuns@naver.com>
2018-04-16 15:07:23 +0900

    Today I learned in mobile programming class 0416

---
+++ fragmentpractice/src/main/java/com/example/hyeon/fragmentpractice/Fragment1.java
@@ -0,0 +1,35 @@
+package com.example.hyeon.fragmentpractice;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by hyeon on 2018-04-16.
+ */
+
+public class Fragment1 extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment1, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Button btnGetText = getActivity().findViewById(R.id.btnGetText);
+        btnGetText.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                TextView lbl = getActivity().findViewById(R.id.lblFragment1);
+                Toast.makeText(getActivity(), lbl.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ fragmentpractice/src/main/java/com/example/hyeon/fragmentpractice/MenuFragment.java
@@ -0,0 +1,24 @@
+package com.example.hyeon.fragmentpractice;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by hyeon on 2018-04-16.
+ */
+/*
+public class MenuFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_menu, container, false);
+
+        return rootView;
+    }
+}
+*/
