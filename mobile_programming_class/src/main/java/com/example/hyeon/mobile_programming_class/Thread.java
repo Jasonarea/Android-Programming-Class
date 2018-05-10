50fb6f1e0ff63db973cb823c05096c44087217ee
Jason Jung <hyeonsuns123@gmail.com>
2018-05-10 15:10:38 +0900

    Today I learned in Class 0510

---
+++ mobile_programming_class/src/main/java/com/example/hyeon/mobile_programming_class/Thread.java
@@ -0,0 +1,57 @@
+package com.example.hyeon.mobile_programming_class;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Thread extends AppCompatActivity {
+    private boolean running;
+    private int value;
+
+    private boolean running;
+    private int value;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView textview = (TextView) findViewById(R.id.textView);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textview.setText("스레드에서 받은 값: " + value);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        running = true;
+        Thread thread1 = new BackgroundThread();
+        thread1.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        running = false;
+        value = 0;
+    }
+
+    class BackgroundThread extends Thread {
+        public void run() {
+            while (running) {
+                try {
+                    Thread.sleep(1000);
+                    value++;
+                } catch (Exception ex) {
+                }
+            }
+        }
+    }
+}
+
